4c66fbfd6905cecaac6ae4e797d32715bf08aaed
Rafael Gomes <rafaelufjf@gmail.com>
2024-11-16 18:13:27 -0300

    alterado index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,6 @@
 function Home() {
     return (
-        <h1 style={{
-            fontFamily: "Arial, sans-serif",
-            color: "#4CAF50",
-            textAlign: "center",
-            marginTop: "50px",
-            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)"
-        }}>
-            Simoninha e Livinha, meus amores
-        </h1>
+        <h1> Chuchua, chuchua, chuchua wawa</h1>
     );
 }
 
