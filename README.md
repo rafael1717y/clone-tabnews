68a8d03f6d148762ea33d46a70e3ca3f1510b9d0
Rafael Gomes <rafaelufjf@gmail.com>
2024-11-18 14:44:27 -0300

    config prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para o curso https://curso.dev .
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 function Home() {
-    return (
-        <h1> Chuchua, chuchua, chuchua wawa</h1>
-    );
+  return <h1> Chuchua, chuchua, chuchua wawa</h1>;
 }
 
-
 export default Home;
+
+function teste() {
+  console.log("ident errada");
+}
